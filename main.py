46a44695dfe434e13304ef3722f2fd31e87c5bd4
stika124 <honza.kessy@gmail.com>
2026-04-30 18:23:50 +0200

    Initialize pygame and add Info class with random method
    Added a class Info with a method to get random values. Initialized pygame and set up a screen for a 2D game. Implemented a function to calculate block pixel positions and added JSON loading for blocks.

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
+import random
+import pygame
+
+class Info:
+  __init__(self):
+    pass
+
+  getRandom(self, f, t):
+    return f + random.random() * (t - f)
+
+DATA = {
+  "blockWidth": 100,
+  "blockSubPixels": 5,
+  "widthScreen": 600,
+  "heigthScreen": 400
+}
+
+pygame.init()
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("2d mc")
+camera = [0, 0]
+world = [
+  {
+    "block": "grass_block",
+    "coor": [0, 0]
+  }
+]
+def getBlockPixels(x, y, subX=0, subY=0):
+  return [(x-camera[0])*DATA["blockWidth"]+subX*(DATA["blockWidth"]/DATA["blockSubPixels"]), 
+          (y-camera[1])*DATA["blockWidth"]+subY*(DATA["blockWidth"]/DATA["blockSubPixels"])]
+
 import json
 with open("blocks.json") as file:
   blocks = json.parse(file.read())
