cde384d94ebef803af913a8fd5024b811b4a85b3
stika124 <honza.kessy@gmail.com>
2026-04-30 22:13:41 +0200

    Refactor world structure to use a dictionary

--- main.py
+++ main.py
@@ -20,12 +20,14 @@ pygame.init()
 screen = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("2d mc")
 camera = [0, 0]
-world = [
-  {
-    "block": "grass_block",
-    "coor": [0, 0]
-  }
-]
+world = {
+  "block": [
+    {
+      "block": "grass_block",
+      "coor": [0, 0]
+    }
+  ]
+}
 def getBlockPixels(x, y, subX=0, subY=0):
   return [(x-camera[0])*DATA["blockWidth"]+subX*(DATA["blockWidth"]/DATA["blockSubPixels"])+DATA["widthScreen"]/2, 
           (y-camera[1])*DATA["blockWidth"]+subY*(DATA["blockWidth"]/DATA["blockSubPixels"])+DATA["heightScreen"]/2]
@@ -40,6 +42,11 @@ while True:
     if event.type == pygame.QUIT:
       running = False
 
+  for block in world["block"]:
+    face = blocks[block["block"]]["face"]
+    for ifL, faceLine in enumerate(face):
+      for ifP, facePiece in enumerate(faceLine):
+        pygame.draw.rect(screen, facePiece[0], getBlockPixels()+[DATA["blockWidth"]/DATA["blockSubPixels"], DATA["blockWidth"]/DATA["blockSubPixels"]])
   screen.fill((255, 255, 255))
   pygame.display.flip()
   clock.tick(60)
